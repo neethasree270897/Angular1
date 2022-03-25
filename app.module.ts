0fdd8a54a096bfa02222ef745f61092a4eec61f8
neethasree270897 <101055469+neethasree270897@users.noreply.github.com>
2022-03-25 12:42:13 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataaService } from './dataa.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [ DataaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
